37b2a84fbdd0ecb91d9ca3cec2fb1874f3b4aef2
Nayeem Mustakim <nmustakim98@gmail.com>
2023-05-30 16:06:04 +0600

    safearea

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -14,23 +14,17 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(elevation: 0,centerTitle:true,title:  const Text("WeatherEasy",style: TextStyle(fontSize: 32),),
-        ),
-        body: SafeArea(
-            child: Container(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.lightBlue,
+      
+          body: SingleChildScrollView(
+              child: Stack(
+                children:[
+                  Image.asset("assets/images/landscape.png", height: MediaQuery.of(context).size.height,fit: BoxFit.fitHeight,),
+                  Column(children: [
+                    const SizedBox(height: 30,),
 
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomLeft,
-                        colors: [Colors.blue, Colors.white])),
-                child: Column(children: [
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  // Note: Same code is applied for the TextFormField as well
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: SizedBox(
@@ -38,17 +32,19 @@ class _HomeState extends State<Home> {
                       child: TextField(
                         controller: _cityController,
                         decoration: InputDecoration(
-                          suffixIcon: InkWell(onTap: () {
-                        Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                        builder: (context) =>
-                        WeatherScreen(_cityController.text)),
-                        );
-                        },child: const Icon(Icons.search,color: Colors.white,)),
+                          suffixIcon:         IconButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          WeatherScreen(_cityController.text)),
+                                );
+                              },
+                              icon: const Icon(Icons.search,color: Colors.white,)),
                           focusColor: Colors.white,
                           focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
+                            borderRadius: BorderRadius.circular(20),
                             borderSide: const BorderSide(
                               color: Colors.white,
                               width: 3,
@@ -65,12 +61,11 @@ class _HomeState extends State<Home> {
                     ),
                   ),
 
+                ]
 
-                  Expanded(child: Image.asset("assets/images/weather-forecast.png",))
-
-                ]),
-
-            )
-        ));
+              ),
+        ]
+              ))),
+    );
   }
 }
